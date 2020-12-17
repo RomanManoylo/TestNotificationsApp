fcf36f5f0e86323b7184205b233313bf01d22610
RomanManoylo <romanmanoylo@gmail.com>
2020-12-17 21:07:27 +0200

    Initial commit

---
+++ app/src/main/java/com/manoilo/testnotificationsapp/view/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.manoilo.testnotificationsapp.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.manoilo.testnotificationsapp.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
